a3cea450dc680afc1cdf2ec98581484b67ace7c5
Wayne Simmerson <wsimmerson@gmail.com>
2023-01-01 08:12:03 -0500

    header, start card and specials

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,27 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>
-                    We are a family owned Mediterranean Restaurant, focused on
-                    trationalal recipes served with a modern twist
-                </p>
-                <Link
-                    to="/reservation"
-                    className="btn"
-                >
-                    Reserve a table
-                </Link>
+            <div className="container">
+                <div>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        We are a family owned Mediterranean Restaurant, focused
+                        on trationalal recipes served with a modern twist
+                    </p>
+                    <Link
+                        to="/reservation"
+                        className="btn"
+                    >
+                        Reserve a table
+                    </Link>
+                </div>
+                <div>
+                    <img
+                        src="/assets/restaurant food B.jpg"
+                        alt=""
+                    />
+                </div>
             </div>
         </header>
     );
---
+++ src/components/Specials.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Specials() {
+    return <section className="specials"></section>;
+}
+
+export default Specials;
